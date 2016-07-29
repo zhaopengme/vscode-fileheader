16042b33d1f62c046529521dd37fa54ea01ccefe
mikey.zhaopeng <mikey.zhao@ndpmedia.com>
2016-07-29 20:11:33 +0800

    update config

--- extension.js
+++ extension.js
@@ -1,13 +1,13 @@
 /*
- * Project: c:\Users\mikey\code\user\vscode-fileheader
- * File: extension.js
+ * @Author: mikey.zhaopeng
+ * @Date:   2016-07-29 15:57:29
+ * @Last Modified by:   mikey.zhaopeng
+ * @Last Modified time: 2016-07-29 16:43:41
  */
 
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 
-Date.prototype.format = function(format) {
+Date.prototype.format = function (format) {
     var o = {
         "M+": this.getMonth() + 1, //month
         "d+": this.getDate(), //day
@@ -17,48 +17,46 @@ Date.prototype.format = function(format) {
         "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
         "S": this.getMilliseconds() //millisecond
     }
-    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
-        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(format))
-            format = format.replace(RegExp.$1,
-                RegExp.$1.length == 1 ? o[k] :
-                ("00" + o[k]).substr(("" + o[k]).length));
+    if (/(y+)/.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
     return format;
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-fileheader" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.fileheader', function() {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
+    var config = vscode.workspace.getConfiguration('fileheader');
+    console.info(config);
+    console.log('"vscode-fileheader" is now active!');
+    var disposable = vscode.commands.registerCommand('extension.fileheader', function () {
         var editor = vscode.editor || vscode.window.activeTextEditor;
-        editor.edit(function(editBuilder) {
-            var time = new Date().format("yyyy-MM-dd hh:mm:ss")
-            var tpl = '/*\r' +
-                ' * @Author: zhaopeng\r' +
-                ' * @Date:   '+time+'\r' +
-                ' * @Last Modified by:   mikey.zhaopeng\r' +
-                ' * @Last Modified time: '+time+'\r' +
-                ' */\r';
+        editor.edit(function (editBuilder) {
+            var time = new Date().format("yyyy-MM-dd hh:mm:ss");
+            var data = {
+                author: config.Author,
+                lastModifiedBy: config.LastModifiedBy,
+                createTime: time,
+                updateTime: time
+            }
+            try {
+            var tpl = new template(config.tpl).render(data);;
+            console.info(tpl);
             editBuilder.insert(new vscode.Position(0, 0), tpl);
+            } catch (error) {
+                console.error(error);
+            }
+
         });
 
     });
 
     context.subscriptions.push(disposable);
-    vscode.workspace.onDidSaveTextDocument(function(file) {
-        setTimeout(function() {
+    vscode.workspace.onDidSaveTextDocument(function (file) {
+        setTimeout(function () {
             try {
                 var f = file;
                 var editor = vscode.editor || vscode.window.activeTextEditor;
@@ -87,7 +85,7 @@ function activate(context) {
                 if ((authorLine != -1) && (lastTimeLine != -1) && (diff > 20)) {
 
                     replaceLineText(authorLine, author, editor);
-                    setTimeout(function() {
+                    setTimeout(function () {
                         replaceLineText(lastTimeLine, lastTime, editor);
                         document.save();
                     }, 200);
@@ -101,7 +99,9 @@ function activate(context) {
     });
 }
 
-
+function getConfiguration() {
+    return vscode.workspace.getConfiguration('mocha');
+}
 
 
 
@@ -127,8 +127,7 @@ function replaceLineText(lineNum, text, editor) {
     const lastLine = document.lineAt(lineNum);
     const end = new vscode.Position(lineNum, lastLine.text.length);
     const range = new vscode.Range(start, end);
-    editor.edit(function(edit) {
-        console.info('--------------');
+    editor.edit(function (edit) {
         edit.replace(range, text);
     });
 
@@ -136,8 +135,40 @@ function replaceLineText(lineNum, text, editor) {
 
 
 
+/**
+ * template engine
+ */
+function template(tpl) {
+    var
+        fn,
+        match,
+        code = ['var r=[];\nvar _html = function (str) { return str.replace(/&/g, \'&amp;\').replace(/"/g, \'&quot;\').replace(/\'/g, \'&#39;\').replace(/</g, \'&lt;\').replace(/>/g, \'&gt;\'); };'],
+        re = /\{\s*([a-zA-Z\.\_0-9()]+)(\s*\|\s*safe)?\s*\}/m,
+        addLine = function (text) {
+            code.push('r.push(\'' + text.replace(/\'/g, '\\\'').replace(/\n/g, '\\n').replace(/\r/g, '\\r') + '\');');
+        };
+    while (match = re.exec(tpl)) {
+        if (match.index > 0) {
+            addLine(tpl.slice(0, match.index));
+        }
+        if (match[2]) {
+            code.push('r.push(String(this.' + match[1] + '));');
+        }
+        else {
+            code.push('r.push(_html(String(this.' + match[1] + ')));');
+        }
+        tpl = tpl.substring(match.index + match[0].length);
+    }
+    addLine(tpl);
+    code.push('return r.join(\'\');');
+    fn = new Function(code.join('\n'));
+    this.render = function (model) {
+        return fn.apply(model);
+    };
+}
+
+
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 exports.deactivate = deactivate;
